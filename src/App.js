8935d5403a3175a052356471cd906d7084ec6ef0
Skyler Burger <skylerburger@gmail.com>
2020-02-09 14:43:59 -0800

    Add Footer, Header, and start of Login components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import Footer from './components/Footer/Footer';
+
 function App() {
   return (
+    <>
+    <Header />
+    <Login />
     <h1>Future Content Coming Soon!</h1>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+const url = 'localhost:8000/accounts/'
+// Add token/ for obtaining a token pair
+// Add create/ to create a user
+
+class Login extends React.Component {
+    render() {
+        return (
+            <p>A Login form</p>
+        )
+    }
+}
+
+export default Login;
