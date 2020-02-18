cfc9996ee178763a9816d44bbd253a3671f012a3
Skyler Burger <skylerburger@gmail.com>
2020-02-17 16:16:41 -0800

    Update MovieList to class component and add sort methods

--- src/components/Movies/MovieList.js
+++ src/components/Movies/MovieList.js
@@ -4,21 +4,55 @@ import { Redirect, Link } from 'react-router-dom';
 import './movies.css';
 
 
-// <MovieList /> Component
-export default props => {
-  // Builds a list of movies if movies are present
-  // Redirects to homepage otherwise
-  if (!props.movies) {
-    return <Redirect to="/" />;
+class MovieList extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      sortFunction: this.runtimeSort,
+      orderAscending: true, 
+    }
+
+    this.runtimeSort = this.runtimeSort.bind(this);
+    this.titleAlphaSort = this.titleAlphaSort.bind(this);
   }
 
-  return (
-    <>
-      { props.movies.map(movie_data => (
-        <MovieItem movie={movie_data} key={movie_data.id}/>
-      ))}
-    </>
-  );
+  titleAlphaSort(a, b) {
+    let sortIndicator = 0;
+    const ascending = this.state.orderAscending;
+    if (a.title < b.title) ascending ? sortIndicator = -1 : sortIndicator = 1;
+    if (a.title > b.title) ascending ? sortIndicator = 1 : sortIndicator = -1;
+    return sortIndicator;
+  }
+
+  runtimeSort(a, b) {
+    let sortIndicator = 0;
+    const ascending = this.state.orderAscending;
+    const runtime_a = parseInt(a.runtime_minutes);
+    const runtime_b = parseInt(b.runtime_minutes);
+    if (runtime_a < runtime_b) ascending ? sortIndicator = -1 : sortIndicator = 1;
+    if (runtime_a > runtime_b) ascending ? sortIndicator = 1 : sortIndicator = -1;
+    return sortIndicator;
+  }
+
+  render () {
+  
+    const movies = this.props.movies.sort(this.state.sortFunction.bind(this));
+
+    return (
+      <>
+        {/* Render movies if there are any in state, or redirect to root */}
+        { this.props.movies ?
+
+          movies.map(movie_data => (
+            <MovieItem movie={movie_data} key={movie_data.id}/>
+          )) :
+
+          <Redirect to="/" />
+      }
+      </>
+    );
+  }
 }
 
 
@@ -42,3 +76,5 @@ const MovieItem = props => {
     </div>
   );
 }
+
+export default MovieList;
