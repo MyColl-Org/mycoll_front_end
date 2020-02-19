9b7524135cc9c35f829e8342b18827f0d8072542
Skyler Burger <skylerburger@gmail.com>
2020-02-18 18:50:20 -0800

    Add ability to delete a movie

--- src/components/Movies/MovieDetail.js
+++ src/components/Movies/MovieDetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 import MovieCopyForm from './MovieCopyForm';
 
@@ -9,10 +10,13 @@ class MovieDetail extends React.Component {
     super(props);
 
     this.state = {
+      confirmDelete: false,
       renderCopyForm: false,
     }
 
     this.copyCreated = this.copyCreated.bind(this);
+    this.deleteMovie = this.deleteMovie.bind(this);
+    this.toggleConfirmDelete = this.toggleConfirmDelete.bind(this);
     this.toggleCopyForm = this.toggleCopyForm.bind(this);
   }
 
@@ -32,21 +36,68 @@ class MovieDetail extends React.Component {
     this.props.addCopy(newCopy);
   }
 
+  toggleConfirmDelete() {
+    let newState = this.state.confirmDelete ? false : true;
+    this.setState({
+      confirmDelete: newState
+    });
+  }
+
+  async deleteMovie() {
+    const axiosConfig = {
+      method: 'delete',
+      url: `http://127.0.0.1:8000/api/v1/movies/${this.props.movie.id}`,
+      data: null,
+      headers: {
+        Authorization: `Bearer ${this.props.accessToken}`
+      }
+    };
+
+    let response;
+    try {
+      response = await axios(axiosConfig);
+    } catch(error) {
+      console.error(error);
+    }
+
+    if (response.status === 204 ) {
+      this.props.onDeleteSuccess(this.props.movie.id);
+    }
+    
+  }
+
+
   render() {
-    // Pre-assemble text content for clarity
-    const movieTitle = `${this.props.movie.title} (${this.props.movie.release_year})`;
-    const movieRating = `MPAA Rating: ${this.props.movie.mpaa_rating}`;
-    const movieRuntime = `Runtime (mins): ${this.props.movie.runtime_minutes}`;
+    let movieTitle, movieRating, movieRuntime;
+
+    if (this.props.movie) {
+      // Pre-assemble text content for clarity
+      movieTitle = `${this.props.movie.title} (${this.props.movie.release_year})`;
+      movieRating = `MPAA Rating: ${this.props.movie.mpaa_rating}`;
+      movieRuntime = `Runtime (mins): ${this.props.movie.runtime_minutes}`;
+    }
     
     return (<>
       { this.props.movie ?
 
         <div className="movie-detail">
           
+          {/* Movie Info OR <Redirect> */}
           <h2>{ movieTitle }</h2>
           <img src={this.props.movie.image_link} alt={`${this.props.movie.title} cover art`} />
           <p>{ movieRating }</p>
           <p>{ movieRuntime }</p>
+
+          {/* Delete Buttons */}
+          { this.state.confirmDelete ?
+            <>
+              <button onClick={this.toggleConfirmDelete}>CANCEL</button>
+              <button onClick={this.deleteMovie}>CONFIRM DELETE</button>
+            </> :
+            <button onClick={this.toggleConfirmDelete}>Delete Movie</button>
+          }
+
+          {/* Copies List */}
           { this.props.movie.copies.length > 0 ?
 
             <>
@@ -56,9 +107,11 @@ class MovieDetail extends React.Component {
               </ul>
             </> :
 
+            // false here renders nothing rather than insert an empty tag
             false
           }
 
+          {/* New Copy Form */}
           { this.state.renderCopyForm ?
           
             <MovieCopyForm 
@@ -71,8 +124,9 @@ class MovieDetail extends React.Component {
           }
         </div> :
 
-        <Redirect to='/' />
+        <Redirect to='/movies' />
       }
+
     </>);
   }
 }
