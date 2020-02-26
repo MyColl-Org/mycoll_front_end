63108afdf81543a96b21c76226daa87e2edb1447
Skyler Burger <skylerburger@gmail.com>
2020-02-25 18:08:21 -0800

    Refactor <App> rendering logic

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends React.Component {
   }
 
   async storeTokens({ access, refresh }) {
+    // Stores JWT pair in state once obtained by <Login>
     this.setState({
       accessToken: access,
       refreshToken: refresh,
@@ -39,14 +40,13 @@ class App extends React.Component {
   }
 
   toggleSignUp() {
-    let newState = true;
-    if (this.state.renderSignUp) newState = false;
-    this.setState({
-      renderSignUp: newState
-    });
+    // Toggles rendering of <SignUp>
+    let newState = this.state.renderSignUp ? false : true;
+    this.setState({ renderSignUp: newState });
   }
 
   greetNewUser(newUser) {
+    // Stores new username to state to customize welcome and login messages
     this.setState({
       newUser: newUser.username,
       renderSignUp: false,
@@ -54,32 +54,32 @@ class App extends React.Component {
   }
 
   render() {
+    const username = this.state.newUser ? ` ${this.state.newUser}` : '';
+    const welcomeMessage = `Welcome${username}, We Have Such Sights To Show You!`;
+    let loginMessage = 'Please Login';
+    if (this.state.newUser) loginMessage += " to Your New Account";
+
     return (
       <div className="app">
         <Router>
           <Header />
           <Switch>
-            <Route path='/' exact>
+            <Route path="/" exact>
               {/* Render collections or login/signup depending on presence of tokens */}
               { this.state.accessToken ?
                 <Collections /> :
                 <>
-                  { this.state.newUser ?
-                    <div className="login">
-                      <h2>Welcome {this.state.newUser}, We Have Such Sights To Show You!</h2>
-                      <p>Please Login to Your New Account</p>
-                      <Login storeTokens={this.storeTokens} /> 
-                    </div> :
-                    <div className="login">
-                      <h2>Welcome, We Have Such Sights To Show You!</h2>
-                      <p>Please Login</p>
-                      <Login storeTokens={this.storeTokens} />
-                    </div>
-                  }
-                  { this.state.renderSignUp ?
-                    <SignUp onSuccess={this.greetNewUser} /> :
-                    <button onClick={this.toggleSignUp}>Sign Up</button>
-                  }
+                  <div className="login">
+                    <h2>{ welcomeMessage }</h2>
+                    <p>{ loginMessage }</p>
+                    <Login storeTokens={this.storeTokens} /> 
+                  </div> 
+                  <div className="signup">
+                    { this.state.renderSignUp ?
+                      <SignUp onSuccess={this.greetNewUser} /> :
+                      <button onClick={this.toggleSignUp}>Sign Up</button>
+                    }
+                  </div>
                 </>
               }
             </Route>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ class Nav extends React.Component {
     return (
       <nav>
         <ul>
-        <li><Link to=''>Home</Link></li>
+          <li><Link to=''>Home</Link></li>
           <li><Link to='/movies'>Movies</Link></li>
           <li><Link to=''>TV Shows</Link></li>
           <li><Link to=''>Books</Link></li>
