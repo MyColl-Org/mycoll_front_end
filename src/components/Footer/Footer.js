f730a9205b679906aa332c5cc2c5860c96bd1e0a
Skyler Burger <skylerburger@gmail.com>
2020-02-27 21:56:44 -0800

    Remove <Header> component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Footer.scss'
 
@@ -6,6 +7,9 @@ class Footer extends React.Component {
   render() {
     return (
       <footer>
+        <Link to="/about">
+          ABOUT <span className="mycoll">MyColl</span>
+        </Link>
         <p>Skyler (c) 2020</p>
       </footer>
     )
